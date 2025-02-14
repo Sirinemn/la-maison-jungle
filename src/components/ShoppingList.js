37b432c9a53e50ab32278239e648769efb609f37
Sirinemn <sirinemn@outlook.fr>
2025-02-14 14:55:34 +0100

    add props to shoppingList

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -2,7 +2,7 @@ import { plantList } from '../datas/plantList'
 import '../styles/ShoppingList.css'
 import PlantItem from './PlantItem'
 
-function ShoppingList() {
+function ShoppingList({cart, updateCart}) {
 	const categories = plantList.reduce(
 		(acc, plant) =>
 			acc.includes(plant.category) ? acc : acc.concat(plant.category),
@@ -10,25 +10,22 @@ function ShoppingList() {
 	)
 
 	return (
-		<div>
-			<ul>
-				{categories.map((cat) => (
-					<li key={cat}>{cat}</li>
-				))}
-			</ul>
-			<ul className='lmj-plant-list'>
-            {plantList.map(({ id, cover, name, water, light }) => (
-					<PlantItem
-						id={id}
-						cover={cover}
-						name={name}
-						water={water}
-						light={light}
-					/>
-				))}
-			</ul>
-		</div>
-	)
+        <div className='lmj-shopping-list'>
+            <ul>
+                {categories.map((cat) => (
+                <li key={cat}>{cat}</li>
+                ))}
+            </ul>
+            <ul className='lmj-plant-list'>
+                {plantList.map(({ id, cover, name, water, light }) => (
+                    <div key={id}>
+                        <PlantItem cover={cover} name={name} water={water} light={light} />
+                        <button onClick={() => updateCart(cart + 1)}>Ajouter</button>
+                    </div>
+                ))}
+            </ul>
+        </div>
+    )
 }
 
 export default ShoppingList
