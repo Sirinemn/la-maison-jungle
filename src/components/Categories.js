894cf7b31efe7da04673ff08173607b204b70c76
Sirinemn <sirinemn@outlook.fr>
2025-02-14 16:25:44 +0100

    fix bugs

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -8,13 +8,13 @@ function Categories({activeCategory, setActiveCategory}) {
         )
         return (
             <div className='lmj-categories'>
-                <select name="categories" id="category-select">
+                <select name="categories" id="category-select" onChange={(e) => setActiveCategory(e.target.value)}>
                     <option value="">--Please choose an option--</option>
                     {categories.map((cat) => (
                         <option 
                             value={cat} 
                             key={cat}
-                            onChange={(e) => setActiveCategory(e.target.value)}
+                            
                             className='lmj-categories-select'
                         >
                             {cat}
